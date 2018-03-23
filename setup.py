7e1ad642a008468d5cafac869cab2451c7469dfa
De Greve Simon <degrevesim@gmail.com>
2018-03-23 17:08:25 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
   name = 'pymediawiki',
   packages = ['pymediawiki'], # this must be the same as the name above
   version = '0.1',
-  description = 'Python Wrapper for MediaWiki's API',
+  description = 'Python Wrapper for MediaWiki API',
   author = 'Simon De Greve',
   author_email = 'degrevesim@gmail.com',
   url = 'https://github.com/Yoiro/pymediawiki', # use the URL to the github repo
