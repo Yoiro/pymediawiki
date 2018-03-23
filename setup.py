a89708d1b2d487d0a969d7c0ec837aecfc6e64ed
De Greve Simon <degrevesim@gmail.com>
2018-03-23 17:07:14 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+  name = 'pymediawiki',
+  packages = ['pymediawiki'], # this must be the same as the name above
+  version = '0.1',
+  description = 'Python Wrapper for MediaWiki's API',
+  author = 'Simon De Greve',
+  author_email = 'degrevesim@gmail.com',
+  url = 'https://github.com/Yoiro/pymediawiki', # use the URL to the github repo
+  download_url = 'https://github.com/Yoiro/pymediawiki/archive/0.1.tar.gz', # I'll explain this in a second
+  keywords = ['mediawiki', 'api', 'wiki'], # arbitrary keywords
+  classifiers = [],
+)
